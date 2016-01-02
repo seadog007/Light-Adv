663cf02531f9787c16d7fa835907ef39140f0ede
Jasper Yu <007seadog@gmail.com>
2016-01-02 21:34:24 +0800

    Auto reset Minecraft map (2)

---
+++ Minecraft/json.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true
+do
+out='{'
+left=`grep 'z:-184,y:63,x:21' Light/logs/latest.log | tail -n1`
+right=`grep 'z:-184,y:63,x:27' Light/logs/latest.log | tail -n1`
+[ -n "`echo $left | grep 'Text1:"0"'`" ] && out=$out'"left": false, '
+[ -n "`echo $left | grep 'Text1:"1"'`" ] && out=$out'"left": true, '
+[ -n "`echo $right | grep 'Text1:"0"'`" ] && out=$out'"right": false'
+[ -n "`echo $right | grep 'Text1:"1"'`" ] && out=$out'"right": true'
+out=$out'}'
+echo $out
+echo $out > light.json
+sleep 1
+done
+
---
+++ Minecraft/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+unzip Light.zip
+cd Light
+java -Xmx1024M -Xms1024M -jar minecraft_server.1.8.8.jar
+rm -rf Light
